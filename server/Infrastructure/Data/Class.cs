d42ccb994677830a18459caa7b21175383431853
Gleb Kapustin <gleb.kapustin@outlook.com>
2025-06-24 12:57:59 +0300

    123

---
+++ server/Infrastructure/Data/Class.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Salary_Insights.Infrastructure.Data
+{
+    public static class DataBaseInitializer
+    {
+        public static void ApplyMigrations(IServiceProvider serviceProvider)
+        {
+            using var scope = serviceProvider.CreateScope();
+            var context = scope.ServiceProvider.GetRequiredService<SalaryInsightsDbContext>();
+
+            // Ожидание доступности базы (например, 30 секунд)
+            var maxRetries = 10;
+            var delay = TimeSpan.FromSeconds(3);
+
+            for (int i = 0; i < maxRetries; i++)
+            {
+                try
+                {
+                    context.Database.OpenConnection();
+                    context.Database.Migrate();
+                    context.Database.CloseConnection();
+                    return;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Ошибка подключения к БД, попытка {i + 1}/{maxRetries}: {ex.Message}");
+                    Thread.Sleep(delay);
+                }
+            }
+
+            throw new Exception("Не удалось подключиться к БД после нескольких попыток.");
+        }
+    }
+}
---
+++ server/Infrastructure/Data/SalaryInsightsDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Salary_Insights.Core.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Salary_Insights.Infrastructure.Data
+{
+    public class SalaryInsightsDbContext : DbContext
+    {
+        public SalaryInsightsDbContext(DbContextOptions<SalaryInsightsDbContext> options) : base(options)
+        {
+        }
+    }
+}
