69164091f14503aee1532ebfbb8271fc8dca1901
Gleb Kapustin <gleb.kapustin@outlook.com>
2025-06-24 21:39:42 +0300

    fix db

--- server/Infrastructure/Data/SalaryInsightsDbContext.cs
+++ server/Infrastructure/Data/SalaryInsightsDbContext.cs
@@ -8,5 +8,22 @@ namespace Salary_Insights.Infrastructure.Data
         public SalaryInsightsDbContext(DbContextOptions<SalaryInsightsDbContext> options) : base(options)
         {
         }
+        public DbSet<Department> Departments { get; set; }
+
+        public DbSet<Employee> Employees { get; set; }
+
+        public DbSet<Position> Positions { get; set; }
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Role> Roles { get; set; }
+
+        public DbSet<Salary> Salarys { get; set; }
+
+        public DbSet<SalaryHistory> SalaryHistory { get; set; }
+
+
+
+
     }
 }
